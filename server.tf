5934ecf77c6964ac9dc85dbce694b678704ee10e
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-24 17:41:08 +0530

    source common code

--- server.tf
+++ server.tf
@@ -1,13 +1,11 @@
-#data "aws_ami" "centos" {
-#  owners = ["973714476881"]
-#  most_recent = true
-#  name_regex = "centos-8-DevOps-practice"
-#}
-#output "ami" {
-#  value = data.aws_ami.centos.image_id
-#}
-
-
+data "aws_ami" "centos" {
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
+output "ami" {
+  value = data.aws_ami.centos.image_id
+}
 
   resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
