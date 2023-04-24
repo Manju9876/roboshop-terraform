5a8834ee5115a81310e160b79cf17bdb7d40137b
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-24 17:01:57 +0530

    source common code

--- server.tf
+++ server.tf
@@ -1,4 +1,14 @@
-resource "aws_instance" "frontend" {
+data "aws_ami" "centos" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "^myami-\\d{3}"
+
+}
+
+output "ami" {
+  value = data.aws_ami.centos.image_id
+}
+  resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
