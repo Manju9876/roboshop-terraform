fed408aecc1e211790a60161c12c89c3b7b951e2
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-05-04 13:18:49 +0530

    d

--- server.tf
+++ server.tf
@@ -10,29 +10,37 @@
       Name = each.value["name"]
     }
 
-     provisioner "remote-exec" {
-        connection {
-          type     = "ssh"
-          user     = "centos"
-          password = "DevOps321"
-          host     = self.private_ip
-       }
 
-       inline = [
-          "rm -rf roboshop-shell",
-          "git clone https://github.com/Manju9876/roboshop-shell.git",
-          "cd roboshop-shell",
-          "sudo bash ${each.value["name"]}"
-       ]
+  }
+  resource "null_resource" "provisioner" {
+
+    depends_on = [aws_instance.instance_name, aws_route53_record.record]
+    for_each   = var.component
+
+    provisioner "remote-exec" {
+
+      connection {
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = aws_instance.instance_name[each.value["name"]].private_ip
+      }
+
+      inline = [
+        "rm -rf roboshop-shell",
+        "git clone https://github.com/Manju9876/roboshop-shell.git",
+        "cd roboshop-shell",
+        "sudo bash ${each.value["name"]}"
+      ]
     }
   }
 
+  resource "aws_route53_record" "record" {
+      for_each = var.component
+      zone_id  = "Z06653061K00JAPLVF5JM"
+      name     = "${each.value["name"]}-dev.manju-devops.online"
+      type     = "A"
+      ttl      = 30
+      records  = [aws_instance.instance_name[each.value["name"]].private_ip]
+  }
 
-resource "aws_route53_record" "record" {
-  for_each = var.component
-  zone_id = "Z06653061K00JAPLVF5JM"
-  name    = "${each.value["name"]}-dev.manju-devops.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance_name[each.value["name"]].private_ip]
-}
