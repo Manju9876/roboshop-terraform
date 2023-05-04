b628eeceb73521539d6dacf29f39ec1d4be0bc6c
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-05-04 21:58:12 +0530

    ss

---
+++ module/main.tf
@@ -0,0 +1,44 @@
+
+resource "aws_instance" "instance_name" {
+
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+
+  tags = {
+    Name = var.component_name
+  }
+
+
+}
+resource "null_resource" "provisioner" {
+
+  depends_on = [aws_instance.instance_name, aws_route53_record.record]
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance_name.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/Manju9876/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${var.component_name}.sh ${var.password}"
+    ]
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id  = "Z06653061K00JAPLVF5JM"
+  name     = "${var.component_name}-dev.manju-devops.online"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance_name.private_ip]
+}
+
--- server.tf
+++ server.tf
@@ -1,46 +1,9 @@
+module "servers" {
+  for_each = var.component
 
-  resource "aws_instance" "instance_name" {
-    for_each = var.component
-    ami           = data.aws_ami.centos.image_id
-    instance_type = each.value["instance_type"]
-    vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-
-
-    tags = {
-      Name = each.value["name"]
-    }
-
-
-  }
-  resource "null_resource" "provisioner" {
-
-    depends_on = [aws_instance.instance_name, aws_route53_record.record]
-    for_each   = var.component
-
-    provisioner "remote-exec" {
-
-      connection {
-        type     = "ssh"
-        user     = "centos"
-        password = "DevOps321"
-        host     = aws_instance.instance_name[each.value["name"]].private_ip
-      }
-
-      inline = [
-        "rm -rf roboshop-shell",
-        "git clone https://github.com/Manju9876/roboshop-shell.git",
-        "cd roboshop-shell",
-        "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password", "null") }"
-      ]
-    }
-  }
-
-  resource "aws_route53_record" "record" {
-      for_each = var.component
-      zone_id  = "Z06653061K00JAPLVF5JM"
-      name     = "${each.value["name"]}-dev.manju-devops.online"
-      type     = "A"
-      ttl      = 30
-      records  = [aws_instance.instance_name[each.value["name"]].private_ip]
-  }
-
+  source = "./module"
+  component = each.value["name"]
+  env = var.env
+  instance_type = each.value["instance_type"]
+  password = lookup(each.value, "password", "null")
+}
