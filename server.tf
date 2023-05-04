6afef1fb6450c238b8dc5869c4156a96ebd062e0
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-05-04 22:11:08 +0530

    sf

--- server.tf
+++ server.tf
@@ -1,8 +1,9 @@
 module "servers" {
   for_each = var.component
 
+
   source = "./module"
-  component = each.value["name"]
+  component_name = each.value["name"]
   env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null")
